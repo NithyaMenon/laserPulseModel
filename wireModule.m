e84c578c134580e57238382b6845b76f3e0499e5
Nithya Menon <nmenon@hmc.edu>
2014-09-27 20:50:40 -0700

    More file descriptions added.

--- wireModule.m
+++ wireModule.m
@@ -1,9 +1,10 @@
 function [ outputPulse ] = wireModule( inputPulse, wireLength )
 %UNTITLED Summary of this function goes here
-%   Inputs: inputPulse [frequency, amplitude, offset, polarization, power]
-%   Outputs: 
+%   Inputs: inputPulse [frequency, amplitude, offset, polarization, power],
+%           wireLength (meters)
+%   Outputs: modulated output pulse
 
-
+inputPulse(3) = wireLength; %% calculate offset here
 outputPulse = inputPulse;
 end
 
