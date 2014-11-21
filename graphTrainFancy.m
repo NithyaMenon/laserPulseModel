149ff7385843d5487f3759d81ba5e226374d0ba1
Alexander Kendrick <akendrick@g.hmc.edu>
2014-11-20 21:25:41 -0800

    Added plotting modules and a diagram with plotting nodes

---
+++ graphTrainFancy.m
@@ -0,0 +1,97 @@
+function graphTrainFancy(inputPulse, stagePulse, desiredPulse, n)
+%   Inputs: inputPulse [ N x 3] matrix containing time, value_vert, and value_horiz in each row
+%   Outputs: plots  time vs. value 
+
+
+tVector = inputPulse(:,1)*10^9;
+
+inVectorVert = inputPulse(:,2);
+inVectorHoriz = inputPulse(:,3);
+stageVectorVert = stagePulse(:,2);
+stageVectorHoriz = stagePulse(:,3);
+desiredVector = desiredPulse(:,2);
+
+
+inVectorVert(inVectorVert <= 0) = 1e-12;
+inVectorHoriz(inVectorHoriz <= 0) = 1e-12;
+stageVectorVert(stageVectorVert <= 0) = 1e-12;
+stageVectorHoriz(stageVectorVert <= 0) = 1e-12;
+desiredVector(desiredVector <= 0) = 1e-12;
+
+fixfonts = @(h) set(h,'FontName','Arial',...
+                      'FontSize',10,...
+                      'FontWeight','bold');
+
+% Here we go, final figure.  Declare the figure
+figure(n);
+% Set the axes
+axis([750 850 0.01 2]);
+% set the y-axis to log
+set(gca,'YScale','log');
+% Turn "hold on" to tell it that we're going to add a couple patches
+hold on
+% Choose the pulse colors, both the lines and the fill
+% Let's do blue and dark red
+linecolors = [0   0 1;   % blue 
+              0.7 0 0; % dark red 
+              0 1 0 ; % green
+              0 0 0 ; % black
+              1 0 1 ; % magenta
+              0 0 1 ; % cyan
+              1 1 1];  % white
+          
+% Let's fill with a lighter color; how light is set by "shading"
+shading = 0.1;
+fillcolors = ones(size(linecolors))*(1-shading)+shading*linecolors;
+% fill in the boxes.  Save the handles h1, h2, for further manipulation.
+h1=fill(tVector,inVectorVert,fillcolors(1,:));
+h2=fill(tVector,inVectorHoriz,fillcolors(2,:));
+h3=fill(tVector,desiredVector,fillcolors(3,:));
+h4=fill(tVector,stageVectorVert,fillcolors(5,:));
+h5=fill(tVector,stageVectorHoriz,fillcolors(6,:));
+
+%h4=fill(tVector,transVector,fillcolors(5,:));
+%alpha(0.5);
+% Make the lines around them thicker and colored
+set(h1,'EdgeColor',linecolors(1,:),'LineWidth',2);
+set(h2,'EdgeColor',linecolors(2,:),'LineWidth',2);
+set(h3,'EdgeColor',linecolors(3,:),'LineWidth',2);
+set(h4,'EdgeColor',linecolors(5,:),'LineWidth',2);
+set(h5,'EdgeColor',linecolors(6,:),'LineWidth',2);
+
+% label with big fonts
+fixfonts(xlabel('Time (ns)'));
+fixfonts(ylabel('Pulse power'));
+fixfonts(title('Final plot'))
+% It may also be nice to have a legend in the NorthEast corner.
+% Note for this we need a vector of handles and a cell-array of strings.
+fixfonts(legend([h1,h2,h4,h5,h3],{'Vertical Input','Horizontal Input','Vertical Intermediate','Horizontal Intermediate', 'Ideal Pulse'},'Location','NorthEast'));
+
+
+% big font labels
+fixfonts(gca);
+% make sure there is a box around the figure
+box on
+
+% That looks much better, doesn't it?  Compare figure(1) to figure(5) to
+% see how far we've come.
+
+% the final trick is that to export it to a presentation, it is best 
+% to first "print" it to a .png.  You can use the "Export Setup" submenu
+% for this, but we've already fixed the fonts.  All we need is to 
+% fix the size and print it.  Let's say we want it to be 4x3 inches.
+% First we set the PaperPosition of the figure, whose handle is either 
+% the figure number or "gcf" for the "current figure"
+% set(5,'units','inches',...         % units could also be pixels, or normalized to the paper size
+%       'PaperPosition',[0.1 0.1 4 3]);  % means put the figure 1 inch from the lower left corner, and make it 2x3
+% % Then we print
+% print(5,'-dpng',... % png format
+%         '-r300',... % resolution 300 dpi
+%         'sample_pulse_plot');  % filename
+    
+% This should generate the file sample_pulse_plot.png, which you can drop
+% directly into PowerPoint, Word, or Acrobat.  
+
+
+
+end
--- plotSimulinkOutput.m
+++ plotSimulinkOutput.m
@@ -1,10 +1,24 @@
-function [ ] = plotSimulinkOutput( sim, totalTime )
-%PLOTSIMULINKOUTPUT Summary of this function goes here
-%   Detailed explanation goes here
+% Matlab script to run model and generate appropriate plots
+
+inputPulse = inputSequence(76.92307692*10^6, 1, 0,1*10^-6);
+options = simset('SrcWorkspace', 'current');
+test = sim('Digitizing_3delays_1us');
+
+graphPulseFancy(inputPulse, 1);
+graphPulseFancy(InitialEOMOut, 2);
+graphPulseFancy(PockelCellOutput, 3);
+graphPulseFancy(PBSDelayOut, 4);
+graphPulseFancy(BSDelayOut, 5);
+graphPulseFancy(OutputPulse, 6);
+
+graphPCFancy(inputPulse, EOMTimings(:,2), OutputPulse, 7);
+graphPCFancy(PockelCellInput, PockelCellTimings(:,2), OutputPulse, 8);
+
+graphTrainFancy(vertPCOut, CombinedPCOut, OutputPulse, 9);
+graphTrainFancy(CombinedPCOut, PBSDelayOut, OutputPulse, 10);
+graphTrainFancy(PBSDelayOut, BSDelayOut, OutputPulse, 11);
+
+graphPCFancy(BSDelayOut, FinalPCTiming(:,2), OutputPulse, 12);
 
 
-data = sim.signals.values;
-pulseSequenceDisplay(data, totalTime);
-
-end
-
+disp(OutputPulse);
