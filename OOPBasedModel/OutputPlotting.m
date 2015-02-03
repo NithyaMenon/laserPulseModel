b4d319b17a8e89f193b9466833bf9b18c6b76215
kunalmenda <kmenda@hmc.edu>
2015-02-02 21:48:46 -0800

    Fixed OOP bug
    Fixed UDD timings in OOP model, altered T2 in idealPulse.m

--- OOPBasedModel/OutputPlotting.m
+++ OOPBasedModel/OutputPlotting.m
@@ -2,6 +2,16 @@ clc
 close all
 
 [ times,verticalPowers,horizontalPowers,widths ] = IDtoPulseData( simout );
+zeropad = zeros(size(times));
+timevec = [ times-widths/2-eps, times-widths/2, times+widths/2,times+widths/2+eps];
+vertvec = [ zeropad, verticalPowers, verticalPowers, zeropad];
+horzvec = [ zeropad, horizontalPowers, horizontalPowers, zeropad];
 
-plot(times,verticalPowers,'k+');
-grid on;
+
+% plot(timevec,vertvec,'r+');
+
+plotdata = transpose([timevec;vertvec;horzvec]);
+[Y,I] = sort(plotdata(:,1));
+plotdata = plotdata(I,:);
+
+graphPulseFancy(plotdata,1);
