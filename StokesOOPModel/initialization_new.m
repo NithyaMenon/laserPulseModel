6979027dc78dcd2a70b5e6992e6cdd1f1f94f380
kunalmenda <kunal.menda@gmail.com>
2015-02-25 19:38:24 -0800

    changes

--- StokesOOPModel/initialization_new.m
+++ StokesOOPModel/initialization_new.m
@@ -6,8 +6,14 @@ clc;
 
 delays = [10e-9, 14.33e-9, 18.66e-9];
 
-PCtimings1 = [0, 39, 104, 169, 221, 273]*1e-9;
-PCtimings2 = [43:2:93, 183:2:243]*1e-9;
+% PCtimings1 = [0, 39, 104, 169, 221, 273]*1e-9;
+% PCtimings2 = [43:2:93, 183:2:243]*1e-9;
+
+PCtimings1 = [-1,1,38,40,103,105,168,170,220,222,272,274]*1e-9;
+PCtimings2 = [43+8,93,183+8,243]*1e-9;
+
+PC1 = PockelsObject(PCtimings1);
+PC2 = PockelsObject(PCtimings2);
 
 
 % Vector of input pulse timings
