65d704de89d620ec37a293f7915e952f2e64356f
LukeStMarie <lukestmarie@gmail.com>
2015-03-11 00:22:32 -0700

    updated documentation
    improved automate documentation

--- automate.m
+++ automate.m
@@ -3,13 +3,31 @@ function [idealTimes, actualTimes, err, passes, eomOnTimes, eomOffTimes, ppEomOn
 %  T - the overall length of the UDD sequence to be approximated, in
 %      nanoseconds
 %  n - the number of pi pulses in the UDD sequence
+%  delTimes - column vector of delay times, in ns. 1st delay should be 0
+%  bestDelays - column vector of delay corresponding to each pulse
+
+%  example input:
+%    delTimes = repRate*[0;1.3334;0.6667]; 
+% note second delay time is greater than the rise time. if the supplied
+% delay 2 is not greater, the repRate will be added to it automatically
+%    bestDelays = [1;3;2;3;2;3;2;1];
+% note delays for pi/2 pulses are included. if this is not done, it
+% will be fixed automatically
+%    T=2028;
+%    N=6;
 
 
-%call delay optimization stuff
 repRate = 13;
+riseTime = 8;
 idealTimes = [0; uddTimes(T,N,0); T];
 offset = 0;
 
+if delTimes(2)<riseTime
+    delTimes(2) = delTimes(2)+repRate;    
+end
+if length(bestDelays)==n
+    bestDelays = [1;bestDelays;1];
+end
 
 
 bestDelTimes = delTimes(bestDelays);
