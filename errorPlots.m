42709a0d291371a01dc381a995ff900ae888dae5
LukeStMarie <lukestmarie@gmail.com>
2015-03-11 22:06:44 -0700

    errorplots documentation
    added a little documentation to errorplots

--- errorPlots.m
+++ errorPlots.m
@@ -1,11 +1,13 @@
-function [ errs, errs2] = errorPlots( x )
+function [ errs, errs2] = errorPlots( placeholder )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 repRate = 13;
 riseTime = 8;
 
+
 T=299:130:2899;
 N=6:2:16;
+
 errs = zeros(length(N),length(T));
 errs2 = zeros(length(N),1);
 i=0;
@@ -16,10 +18,11 @@ for n=N
     for t=T
         j=j+1;
         [~, delTimes, bestDelays, ~] = delOp(t,n,0);
-        [eom2OnT, eom2OffT, eom1OnT, eom1OffT,seqFail,rms]...
-            = automate(t,n,delTimes,bestDelays);
+        %err is given by the function in automate now,which
+        %is currently a (1+RMS/WT)*SFE
+        [~, ~, ~, ~,seqFail,err]= automate(t,n,delTimes,bestDelays);
         if seqFail==0
-            errs(i,j)=rms;
+            errs(i,j)=err;
         end
     end
     errs2(i) = sum(errs(i,:))/nnz(errs(i,:));
