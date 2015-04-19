93c9b36c3dfd14cd2a73a2b728d4484e2c2e9bb4
kunalmenda <kunal.menda@gmail.com>
2015-04-19 14:00:41 -0700

    tweaked TimingPerformance

--- Experimental_MonteCarlo/calculateTimingPerformance.m
+++ Experimental_MonteCarlo/calculateTimingPerformance.m
@@ -3,7 +3,7 @@ function [TimingPerformance, MSE] = calculateTimingPerformance(ImportantPulses_I
     
 
     T = T*1e9;
-    target = 17.317171337233528; % Change. This is only for N=6
+%     target = 17.317171337233528; % Change. This is only for N=6
     timings = transpose((ImportantPulses_times - ImportantPulses_times(1))*1e9);
     
     ff = @(w,timings) abs(1+(-1)^(n+1)*exp(1i*w*T) + ...
@@ -11,9 +11,14 @@ function [TimingPerformance, MSE] = calculateTimingPerformance(ImportantPulses_I
     lorentzian = @(w) 10^6*2/pi./(1+(w*10^6).^2);
     
     
-    TimingPerformance = integral(@(w)ff(w,timings(2:end-1)).*lorentzian(w)./w.^2,0,target/T);
-
     idealPulseTimings = uddTimes(T,n);
+    w = logspace(-6,8,1000);
+    [~,uLimInd] = max(ff(w,idealPulseTimes)./w.^2);
+    uLim = w(uLimInd);
+    
+    
+    TimingPerformance = integral(@(w)ff(w,timings(2:end-1)).*lorentzian(w)./w.^2,0,uLim);
+
     
     % Add the initial offset to the ideal pulse times
     idealPulseTimings = idealPulseTimings + timings(1);
