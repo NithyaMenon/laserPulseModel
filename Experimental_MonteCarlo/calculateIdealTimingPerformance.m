d882348cb50481aaed1acfc051b4be661a043303
JustinCaseling <pjerger@hmc.edu>
2015-04-21 22:54:49 -0700

    Deleted unnecessary code
    Also removed a bit of whitespace

--- Experimental_MonteCarlo/calculateIdealTimingPerformance.m
+++ Experimental_MonteCarlo/calculateIdealTimingPerformance.m
@@ -3,15 +3,13 @@ function idealTimingPerformance = calculateIdealTimingPerformance(n,T)
 
     T = T*1e9;
     
-    idealPulseTimes = uddTimes(T,n);
-    idealTimings = idealPulseTimes;
-    
+    idealTimings = uddTimes(T,n);
         
     ffIdeal =  @(w,idealTimings) abs(1+(-1)^(n+1)*exp(1i*w*T) + ...
         sum(2*exp(1i*bsxfun(@plus,(1:n)'*pi,idealTimings*w)),1)).^2;
     
     w = logspace(-6,8,1000);
-    [~,uLimInd] = max(ffIdeal(w,idealPulseTimes)./w.^2);
+    [~,uLimInd] = max(ffIdeal(w,idealTimings)./w.^2);
     uLim = w(uLimInd);
     
     lorentzian = @(w) 10^6*2/pi./(1+(w*10^6).^2);
