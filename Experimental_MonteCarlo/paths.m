cfa258334d2187bab616283090b77a040cafcab2
kunalmenda <kunal.menda@gmail.com>
2015-05-08 13:57:54 -0700

    cleanup

--- Experimental_MonteCarlo/paths.m
+++ Experimental_MonteCarlo/paths.m
@@ -1,2 +1,5 @@
-pat = fileparts(pwd());
-addpath(strcat(pat,'/Experimental_MonteCarlo/Components'));
+% pat = fileparts(pwd());
+% addpath(strcat(pat,'/Experimental_MonteCarlo/Components'));
+
+% Add paths here using code like the code above if you would like to
+% automated the process of adding paths.
